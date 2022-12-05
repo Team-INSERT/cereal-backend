d14977e7a2902e021e6e88269fea7c72e176ddfb
Ubinquitous <ubinquitous1@gmail.com>
2022-12-05 20:03:22 +0900

    ADD : roomt DB 작성

---
+++ database/models/room.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    const room = sequelize.define("room", {
+        roomId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true
+        }
+    }, {
+        timestamps: true,
+        underscored: false,
+        modelName: 'room',
+        tableName: 'room',
+        paranoid: false,
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+    });
+    room.associate = model => {
+        room.belongsTo(model.user, { foreignKey: "code", sourceKey: 'code' });
+    };
+
+
+    return room;
+};
