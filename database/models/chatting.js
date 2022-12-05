c6e76ba26e3246a4879c275e8dec466a41e7f9c6
Ubinquitous <ubinquitous1@gmail.com>
2022-12-05 20:03:11 +0900

    ADD : Chatting DB 작성

---
+++ database/models/chatting.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+    const chatting = sequelize.define("chatting", {
+        contents: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+    }, {
+        timestamps: true,
+        underscored: false,
+        modelName: 'chatting',
+        tableName: 'chatting',
+        paranoid: false,
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+    });
+    chatting.associate = function (model) {
+        model.chatting.belongsTo(model.user, {
+            foreignKey: 'code',
+            sourceKey: 'code'
+        });
+    };
+    return Board;
+}
