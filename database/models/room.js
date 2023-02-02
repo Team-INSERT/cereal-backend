414517d0ce0a65836b49b9dfd625109730cd4895
Ubinquitous <ubinquitous1@gmail.com>
2023-02-02 11:29:30 +0900

    ADD : Socket

--- database/models/room.js
+++ database/models/room.js
@@ -1,22 +1,25 @@
 module.exports = (sequelize, DataTypes) => {
-    const room = sequelize.define("room", {
-        roomId: {
-            type: DataTypes.INTEGER,
-            primaryKey: true
-        }
-    }, {
-        timestamps: true,
-        underscored: false,
-        modelName: 'room',
-        tableName: 'room',
-        paranoid: false,
-        charset: 'utf8',
-        collate: 'utf8_general_ci',
-    });
-    room.associate = model => {
-        room.belongsTo(model.user, { foreignKey: "code", sourceKey: 'code' });
-    };
+	const room = sequelize.define(
+		'room',
+		{
+			roomId: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+			},
+		},
+		{
+			timestamps: true,
+			underscored: false,
+			modelName: 'room',
+			tableName: 'room',
+			paranoid: false,
+			charset: 'utf8',
+			collate: 'utf8_general_ci',
+		}
+	)
+	room.associate = (model) => {
+		room.belongsTo(model.user, { foreignKey: 'code', sourceKey: 'code' })
+	}
 
-
-    return room;
-};
+	return room
+}
--- src/main.js
+++ src/main.js
@@ -1,33 +1,56 @@
-const express = require('express');
+const dotenv = require('dotenv')
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const http = require('http')
+const { Server } = require('socket.io')
 
-const dotenv = require('dotenv');
-const cors = require('cors');
-const io = require('socket.io')(http);
+dotenv.config()
 
-const { sequelize } = require('../database/models');
+const { sequelize } = require('../database/models')
+const { NotFoundException } = require('./global/exception')
 
-dotenv.config();
-const app = express();
+const app = express()
+const controller = require('./domain/controller')
+const server = http.createServer(app)
+const io = new Server(server, {
+	cors: {
+		origin: '*',
+		methods: ['GET', 'POST'],
+	},
+})
 
-app.use(cors());
+app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
 
-sequelize.sync({ force: false })
-    .then(() => {
-        console.log("DB Connect");
-    })
-    .catch((err) => {
-        console.error(err);
-    })
+// app.use('/api', controller)
 
-io.on('connection', socket => {
-    console.log(socket.id, 'Connected');
+// app.use((req, res, next) => {
+// 	next(new NotFoundException())
+// })
 
-    socket.emit('msg', `${socket.id} 연결 되었습니다.`);
-    socket.on('msg', data => {
-        console.log(socket.id, data);
-        socket.emit('msg', `Server : "${data}" 받았습니다.`);
-    });
-});
+sequelize
+	.sync({ force: false })
+	.then(() => {
+		console.log('DB Connect')
+	})
+	.catch((err) => {
+		console.error(err)
+	})
 
-app.listen(8000);
+io.on('connection', (socket) => {
+	console.log('New client connected')
+
+	socket.on('disconnect', () => console.log('user disconnect', socket.id))
+
+	socket.on('good', (data) => {
+		console.log(data) // 클라이언트 -> 서버
+	})
+
+	setInterval(() => {
+		socket.emit('hi', '서버 -> 클라이언트')
+	}, 3000)
+})
+
+app.listen(8081)
