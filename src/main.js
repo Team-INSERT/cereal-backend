5783b85fc03ebe774e2e9cfb9618445c82536001
Ubinquitous <ubinquitous1@gmail.com>
2023-02-03 10:02:30 +0900

    ADD : room join, find ip

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const http = require('http')
 const { Server } = require('socket.io')
 const { sequelize } = require('../database/models')
-
 
 const app = express()
 const server = http.createServer(app)
@@ -22,8 +21,8 @@ app.use(cookieParser())
 
 // app.use('/api', controller)
 
- app.use((req, res, next) => {
-	 res.send('404')
+app.use((req, res, next) => {
+	res.send('404')
 })
 
 try {
@@ -32,9 +31,35 @@ try {
 } catch (err) {
 	console.error(err)
 }
+
 io.on('connection', (socket) => {
-	socket.on('message', (message) => {
-		console.log(message)
+	const req = socket.request
+
+	const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+	console.log(`new client connected ip ${ip}, socket id : ${socket.id}, date : ${new Date()}`)
+
+	socket.on('join', function (data) {
+		socket.join(data)
+		io.to(data).emit('welcome')
+	})
+
+	socket.on('disconnect', () => {
+		console.log(`client disconnected ip ${ip}, socket id : ${socket.id}, date : ${new Date()}`)
+	})
+
+	socket.on('error', (error) => {
+		console.error(error)
+	})
+
+	socket.on('message', (data) => {
+		const date = new Date()
+		date.setHours(date.getHours() + 9)
+
+		io.to(data.roomId).emit('message', {
+			socketId: socket.id,
+			message: data.message,
+			date: date,
+		})
 	})
 })
 
